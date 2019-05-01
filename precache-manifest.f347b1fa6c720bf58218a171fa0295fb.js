e3b2d94201b718cba11ad53282bbc389cbb00332
Yujie Shao <kristyshao1218@gmail.com>
2019-04-30 20:12:47 -0500

    Updates

---
+++ precache-manifest.f347b1fa6c720bf58218a171fa0295fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd45faf1f3b55d0210d2",
+    "url": "/cs410final-project-front-end/static/js/runtime~main.5f1434cc.js"
+  },
+  {
+    "revision": "fee1305879d7c04e969d",
+    "url": "/cs410final-project-front-end/static/js/main.0046a713.chunk.js"
+  },
+  {
+    "revision": "87e8da0dfcc52c37a9d2",
+    "url": "/cs410final-project-front-end/static/js/2.80c6baa4.chunk.js"
+  },
+  {
+    "revision": "fee1305879d7c04e969d",
+    "url": "/cs410final-project-front-end/static/css/main.63446ccf.chunk.css"
+  },
+  {
+    "revision": "d20e66890cca9af203e71a68cf154f02",
+    "url": "/cs410final-project-front-end/index.html"
+  }
+];
